66ed2a52b8df5a9780f28828a01b57ac524ff2d0
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-19 17:05:24 +0200

    change

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,10 @@ sonarqube {
     properties {
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.organization", "beforeigners")
-//        property("sonar.projectKey", "beforeigners_authorization-manager-sdk-kotlin")
-//        property("sonar.sources", "src/main")
-//        property("sonar.tests", "src/test")
-//        property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
+        property("sonar.projectKey", "beforeigners_authorization-manager-sdk-kotlin")
+        property("sonar.sources", "src/main/kotlin")
+        property("sonar.tests", "src/test/kotlin")
+        property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
 
