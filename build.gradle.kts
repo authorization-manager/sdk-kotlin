c86d2b35c0d589c1cedd7c4ac097afff12decd7c
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-19 21:00:44 +0200

    spotless (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,31 @@ plugins {
     `java-library`
     jacoco
     id("org.sonarqube") version "2.8"
+    id("com.diffplug.gradle.spotless") version "3.28.1"
 }
+
+// spotless configuration -- BEGIN
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+}
+
+tasks.spotlessCheck {
+    mustRunAfter(tasks.classes, tasks.testClasses)
+}
+
+tasks.test {
+    mustRunAfter(tasks.spotlessCheck)
+}
+
+tasks.check {
+    dependsOn(tasks.spotlessCheck)
+}
+// spotless configuration -- END
 
 // JaCoCo configuration -- BEGIN
 jacoco {
@@ -33,6 +57,35 @@ sonarqube {
     }
 }
 // SonarQube configuration -- END
+
+tasks.withType<Test> {
+    testLogging {
+        showStandardStreams = false
+        events("skipped", "failed")
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+    afterSuite(printTestResult)
+}
+
+val printTestResult: KotlinClosure2<TestDescriptor, TestResult, Void>
+    get() = KotlinClosure2({ desc, result ->
+
+        if (desc.parent == null) { // will match the outermost suite
+            println("------")
+            println(
+                    "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} " +
+                            "successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)"
+            )
+            println(
+                    "Tests took: ${result.endTime - result.startTime} ms."
+            )
+            println("------")
+        }
+        null
+    })
 
 repositories {
     jcenter()
--- src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
+++ src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
@@ -1,15 +1,16 @@
 package com.davidgracia.software.authorizationmanager.sdk.kotlin
 
+import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class LibraryTest {
     @Test
     fun testSomeLibraryMethod() {
+        UUID.randomUUID()
         val user = User("1234")
         user.boo()
         AuthorizationManager().save(user)
         assertEquals(expected = user, actual = user)
     }
 }
-
