215d626c7fd45b9de127c854ff917600b35307e9
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-21 01:14:18 +0200

    change (#17)

--- src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
+++ src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
@@ -1,16 +1,23 @@
 package com.davidgracia.software.authorizationmanager.sdk.kotlin
 
-import java.util.UUID
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import java.net.URI
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 
 class LibraryTest {
+
+    private val authorizationManager = AuthorizationManager(URI("http://localhost:8080/users"))
+    private val identifier = "1234"
+    private val name = "John"
+    private val user = User(identifier = identifier, name = name)
+    private val expectedUser = User(identifier = identifier, name = name)
+
+    @Disabled
     @Test
     fun testSomeLibraryMethod() {
-        UUID.randomUUID()
-        val user = User("1234")
-        user.boo()
-        AuthorizationManager().save(user)
-        assertEquals(expected = user, actual = user)
+        val createdUser: User = authorizationManager.save(user)
+
+        assertThat(createdUser).usingRecursiveComparison().ignoringFields("identifier").isEqualTo(expectedUser)
     }
 }
