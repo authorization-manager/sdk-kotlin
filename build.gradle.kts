c2167fe177da97d3d025b865205ae74c25e05783
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-21 19:40:13 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,6 +118,7 @@ allprojects {
 
         "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.6.2")
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+        "testImplementation"("io.mockk:mockk:1.10.0")
         "testImplementation"("org.assertj:assertj-core:3.15.0")
     }
 }
