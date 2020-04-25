18bafbe4a1ace066fd5166504e1c6800703300a2
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-04-26 00:08:45 +0200

    change

--- src/main/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/AuthorizationManager.kt
+++ src/main/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/AuthorizationManager.kt
@@ -7,6 +7,23 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 data class AuthorizationManager(val host: URI) {
+    fun getUser(identifier: String): User {
+        val httpClient: HttpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build()
+
+        val requestBody = """{"query":"{getUser(identifier: \"$identifier\") {\n    identifier\n  }\n}","variables":{}}"""
+        val httpRequest: HttpRequest =
+                HttpRequest
+                        .newBuilder()
+                        .uri(host)
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                        .build()
+
+        val response: HttpResponse<String> = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString())
+
+        return User("asdsdaf", "asddfa")
+    }
+
     fun save(user: User): User {
         val httpClient: HttpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build()
 
@@ -31,3 +48,42 @@ data class AuthorizationManager(val host: URI) {
         return Gson().toJson(this)
     }
 }
+
+/*
+request
+mutation {
+  createUser(user: { name: "Carlos" }) {
+    name
+    identifier
+  }
+}
+
+response
+{
+  "data": {
+    "createUser": {
+      "name": "Carlos",
+      "identifier": "fc586ad9-fe83-4c7e-92b2-c4e44cfbfb04"
+    }
+  }
+}
+----------------------------------------------------------
+request
+query {
+  getUser(identifier: "1") {
+    identifier
+    name
+  }
+}
+
+response
+{
+  "data": {
+    "getUser": {
+      "identifier": "1",
+      "name": "John"
+    }
+  }
+}
+
+*/
--- src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
+++ src/test/kotlin/com/davidgracia/software/authorizationmanager/sdk/kotlin/LibraryTest.kt
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test
 
 class LibraryTest {
 
-    private val authorizationManager = AuthorizationManager(URI("http://localhost:8080/users"))
+//    private val authorizationManager = AuthorizationManager(URI("http://localhost:8080/users"))
+    private val authorizationManager = AuthorizationManager(URI("http://localhost:8080/graphql"))
     private val identifier = "1234"
     private val name = "John"
     private val user = User(identifier = identifier, name = name)
@@ -19,5 +20,13 @@ class LibraryTest {
         val createdUser: User = authorizationManager.save(user)
 
         assertThat(createdUser).usingRecursiveComparison().ignoringFields("identifier").isEqualTo(expectedUser)
+    }
+
+    @Disabled
+    @Test
+    fun testSomeLibraryMethod2() {
+        authorizationManager.getUser("1")
+
+//        assertThat(createdUser).usingRecursiveComparison().ignoringFields("identifier").isEqualTo(expectedUser)
     }
 }
